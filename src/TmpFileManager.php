ca9403348555227920e7c65892b656faf3861272
Aleksandr Denisyuk <denisyukphp@gmail.com>
2020-05-02 18:29:53 +0300

    Renamed addEventListeners() to initEventListeners()

--- src/TmpFileManager.php
+++ src/TmpFileManager.php
@@ -39,13 +39,13 @@ final class TmpFileManager
         $this->tmpFileHandler = $tmpFileHandler ?? new TmpFileHandler(new Filesystem());
         $this->eventDispatcher = $eventDispatcher ?? new EventDispatcher();
 
-        $this->addEventListeners();
+        $this->initEventListeners();
 
         $this->eventDispatcher->dispatch(new DeferredPurgeEvent($this));
         $this->eventDispatcher->dispatch(new GarbageCollectionEvent($this->config));
     }
 
-    private function addEventListeners(): void
+    private function initEventListeners(): void
     {
         $this->eventDispatcher->addListener(DeferredPurgeEvent::class, new DeferredPurgeListener());
         $this->eventDispatcher->addListener(GarbageCollectionEvent::class, new GarbageCollectionListener());
