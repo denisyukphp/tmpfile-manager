8fcc44ebd60fd1fa24059f4429ea2a48f65a80b5
Aleksandr Denisyuk <rb4@inbox.ru>
2022-09-13 11:51:53 +0300

    Upgrade package

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,19 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in([
+        __DIR__.'/src/',
+        __DIR__.'/tests/',
+    ])
+;
+
+return (new PhpCsFixer\Config())
+    ->setUsingCache(true)
+    ->setCacheFile(__DIR__.'/build/cache/php-cs-fixer.cache')
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+    ])
+    ->setFinder($finder)
+;
